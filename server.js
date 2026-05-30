a33fb5a8dc1632fa73f7f42e9201aa3a2786281b
OMARFAROOQ20 <omarfarooqsk2004@gmail.com>
2026-05-30 21:25:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,25 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// 🌙 Static files (frontend)
 app.use(express.static("public"));
 
+// 📿 Routes
 const prayerRoute = require("./routes/prayer");
 const quranRoute = require("./routes/quran");
+const contentRoute = require("./routes/content");
 
+// API endpoints
 app.use("/prayer", prayerRoute);
 app.use("/quran", quranRoute);
+app.use("/content", contentRoute);
 
+// 🌐 Home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// 🚀 Start server
 app.listen(PORT, () => {
-  console.log("Server started successfully");
+  console.log("🌙 Lights of Imaan server running on port " + PORT);
 });
